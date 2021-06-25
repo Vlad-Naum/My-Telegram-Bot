a2b86d11e8be4293923dbe63a14b51d2088a65ee
Vlad <naumkin.vlad2012@yandex.ru>
2021-06-25 10:09:37 +0300

    added SpringBoot skeleton in the programm.

---
+++ src/main/java/com/github/VladNaum/MyTelegramBot/MyTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.VladNaum.MyTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/VladNaum/MyTelegramBot/MyTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.VladNaum.MyTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
